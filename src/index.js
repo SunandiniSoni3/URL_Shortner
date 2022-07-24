4c53ae83b2aa6ddeb9008ee69db4c70b0174b95d
SunandiniSoni3 <102989520+SunandiniSoni3@users.noreply.github.com>
2022-07-24 13:11:02 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ const mongoose= require("mongoose")
 
 const app= express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb+srv://Uranium-Batch:aruSjkdGdfhc9MRK@functionup.eel5r.mongodb.net/group16Database", {
     useNewUrlParser: true
